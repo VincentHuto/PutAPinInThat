d5c89e16614d1d76e3394feed680188f0fbd46a8
Vincent Huto <amae804@gmail.com>
2026-09-13 19:28:32 -0500

    Remove MnA Stuff

--- src/main/java/com/vincenthuto/putapinthat/client/ClientBootstrap.java
+++ src/main/java/com/vincenthuto/putapinthat/client/ClientBootstrap.java
@@ -5,7 +5,6 @@ import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -20,21 +19,7 @@ public final class ClientBootstrap {
         modBus.addListener(ClientBootstrap::registerOverlays);
         loadingContext.registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC);
         MinecraftForge.EVENT_BUS.register(ClientEvents.class);
-        initializeOptionalIntegration("mna",
-                "com.vincenthuto.putapinthat.compat.mna.MnAGuidebookIntegration");
         PinnedRecipeManager.getInstance().load();
-    }
-
-    private static void initializeOptionalIntegration(String modId, String integrationClassName) {
-        if (!ModList.get().isLoaded(modId)) {
-            return;
-        }
-
-        try {
-            Class.forName(integrationClassName).getMethod("initialize").invoke(null);
-        } catch (ReflectiveOperationException exception) {
-            PutAPinInThat.LOGGER.error("Could not initialize {} integration", modId, exception);
-        }
     }
 
     private static void registerKeys(RegisterKeyMappingsEvent event) {
